bc893c65f425bb7cd539d100a659e3ef53c2bc73
Ubuntu <ubuntu@ip-172-31-16-144.us-west-2.compute.internal>
2020-03-10 08:57:39 +0000

    New changes to App.java

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  My Code" );
+        System.out.println( "Hello World  My Code from jenkins" );
     }
 
 	public int calculateSomething() {
